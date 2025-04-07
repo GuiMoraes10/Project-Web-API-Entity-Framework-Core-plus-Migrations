d9c8b29e448ccc78f24909c8895cfa7a0d5d8546
Guilherme de Oliveira Moraes <moraes@alfainstrumentos.com.br>
2025-04-07 12:03:27 -0300

    Adding AppDbContext class

---
+++ APICatalogo/APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+// DbContext - Representa uma sessao com o banco de dados sendo a ponte entre as entidades de dominio e o banco
+// DbSet<T> - Representa uma colecao de entidades no contexto que podem ser consultadas no banco de dados
+
+// DbContext e responsavel realizar a comunicacao entre as entidades e o banco de dados, gerenciando interacoes, consultas, atualizacoes
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    // Representa uma tabela no banco de dados que deseja mapear
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
--- APICatalogo/APICatalogo/obj/Debug/net9.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net9.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9db82d918b20f9025b2667cf743112b5020788")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b851da08f371f45ee401d85c70dad402f104a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
