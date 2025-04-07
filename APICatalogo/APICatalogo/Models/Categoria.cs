bc3f1328ebb7eda645ed0c7b24365f324c9f571a
Guilherme de Oliveira Moraes <moraes@alfainstrumentos.com.br>
2025-04-07 14:31:10 -0300

    Defining entities relationship

--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,8 +1,21 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
 
 public class Categoria
 {
+
+    // Inicializando a colecao por boa pratica
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+
+    // Indica que a categoria tera produtos nela, definindo a relacao de um para muitos.
+    // Ja seria o suficiente para estabelecer a relacao, sem necessariamente precisar alterar a classe Produto.
+    public ICollection<Produto>? Produtos { get; set; }
 }
--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -9,4 +9,10 @@ public class Produto
     public string? ImagemUrl {  get; set; }
     public float Estoque {  get; set; }
     public DateTime DataCadastro { get; set; }
+
+    // Explicita o Id que sera criado como Foreign Key no banco de dados.
+    public int CategoriaId {  get; set; }
+
+    // Explicita que o Produto ira pertencer a uma Categoria.
+    public Categoria? Categoria { get; set; }
 }
