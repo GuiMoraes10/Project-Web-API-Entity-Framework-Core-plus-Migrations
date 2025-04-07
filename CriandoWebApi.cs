c8a85ce3f5aef5eee61d2167d69c82f36a8e5b8e
Guilherme de Oliveira Moraes <moraes@alfainstrumentos.com.br>
2025-04-07 10:44:32 -0300

    First commit

---
+++ CriandoWebApi.cs
@@ -0,0 +1,8 @@
+// Criar uma Web API para um catalogo de produtos/categorias que podem atender
+// uma rede de lojas ou supermercados e tambem consumidores finais.
+
+// Criar um servico RESTFul que permite que aplicativos clientes
+// acessem e gerenciem o catalogo de produtos e categorias.
+
+// Expor endpoints para criar, ler, editar e excluir produtos/categorias
+// e tambem consultar produtos/categorias usando os verbos HTTP e os codigos de status.
